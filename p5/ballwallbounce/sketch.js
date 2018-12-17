52b51825accab3c35b708d048e7238c8e1aae58f
matt <27matt37@gmail.com>
2018-12-17 14:33:44 +0000

    touch create balls

--- p5/ballwallbounce/sketch.js
+++ p5/ballwallbounce/sketch.js
@@ -1,15 +1,9 @@
-let y = 200;
-let x = 5;
-let stepx = 2;
-let stepy = 2;
+let balls = [];
+
 function setup() {
   // put setup code here
   createCanvas(400,400);
   background(230);
-  text('I love you Katie', 10, 60);
-  fill(0, 102, 153, 51);
-  let x = 20;
-  let y = 20;
   frameRate(60);
 
 }
@@ -18,20 +12,42 @@ function draw() {
   // put drawing code here
   fill(20);
   background(230);
-  rect(x,y, 60, 60, 50);
-  text(x, 20, 20);
-  text(y, 20, 50);
-  if (y >= 400 -60 ){
-  stepy *=-1;
-  }else if (y <= 0){
-    stepy*=-1;
-  }
-  if (x >= 400 - 60){
-  stepx *= -1;
-  }else if (x<=0){
-  stepx *= -1;
-  }
-  y+= stepy;
-  x+=stepx;
+
+
+
+  balls.forEach(ball => ball.draw());
 
 }
+
+function Ball(xcoord, ycoord){
+this.size = 30;
+this.xcoord = xcoord;
+this.ycoord = ycoord;
+this.stepx =2;
+this.stepy =2;
+this.r = random(255);
+this.g = random(255);
+this.b = random(255);
+this.draw = function () {
+ fill(this.r,this.g,this.b);
+ rect(this.xcoord, this.ycoord, this.size, this.size, this.size);
+
+ if (this.ycoord >= 400 -this.size ){
+  this.stepy *=-1;
+  }else if (this.ycoord <= 0){
+    this.stepy*=-1;
+  }
+  if (this.xcoord >= 400 - this.size){
+  this.stepx *= -1;
+  }else if (this.xcoord<=0){
+  this.stepx *= -1;
+  }
+  this.ycoord+= this.stepy;
+  this.xcoord+=this.stepx;
+
+}
+
+}
+ function touchEnded(){
+  balls.push(new Ball(mouseX, mouseY));
+  }
