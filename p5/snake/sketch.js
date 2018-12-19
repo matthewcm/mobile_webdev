3142335064584a269d97ce0d50534f16af399cb1
matt <27matt37@gmail.com>
2018-12-19 21:40:20 +0100

    On screen controls

--- p5/snake/sketch.js
+++ p5/snake/sketch.js
@@ -12,6 +12,12 @@ function setup() {
  snake = new Snake();
   snake.addCell();
   food = new Cell(50,50,null, true);
+  buttons = new Controls();
+  buttons.addButton("LEFT");
+  buttons.addButton("RIGHT");
+  buttons.addButton("UP");
+  buttons.addButton("DOWN");
+
   ticksCap = 30;
   ticks = 0;
 }
@@ -20,33 +26,67 @@ function draw() {
   // put drawing code here
   fill(20);
   background(230);
-text("hi",10,10);
 
   snake.draw();
   food.draw();
-  console.log(snake);
-
-  if (mouseIsPressed){
-  text("down",4,20);
-  if (mouseX < 200&& mouseY <200 ){
-  snake.changeDir("LEFT");
-  }else if (mouseX > 200&& mouseY <200 ){
-  snake.changeDir("RIGHT");
-  }if (mouseX < 200&& mouseY >200 ){
-  snake.changeDir("DOWN");
-  }if (mouseX > 200&& mouseY >200 ){
-  snake.changeDir("UP");
-  }
-  }
+  buttons.draw();
+  buttons.checkPressed();
 
 //  text(keyboardDown, 10,10);
 }
+function Controls(){
+
+this.buttons = [];
+
+this.addButton = function(dir){
+    this.buttons.push(new Button(dir));
+};
+this.draw = function(){
+    this.buttons.forEach(button => button.draw());
+};
+this.checkPressed = function(){
+     if (mouseIsPressed){
+       this.buttons.forEach(button => {
+       if (mouseX > button.x && mouseX < button.x + button.size && mouseY > button.y && mouseY < button.y + button.size){
+            snake.changeDir(button.dir);
+       }
+         }
+     );
+     }
+ }
+}
+ function Button (dir){
+ this.dir = dir;
+ this.size = 50;
+ if (dir === "LEFT"){
+    this.x = 250;
+    this.y = 300;
+    this.colour = "rgb(255,0,0)";
+ }else  if (dir === "RIGHT"){
+    this.x = 350;
+    this.y = 300;
+    this.colour = "rgb(255,0,0)";
+ }else if (dir === "UP"){
+    this.x = 300;
+    this.y = 250;
+    this.colour = "rgb(0,255,0)";
+ } else if (dir === "DOWN"){
+    this.x = 300;
+    this.y = 350;
+    this.colour = "rgb(0,255,0)";
+ }
+ this.draw = function() {
+    fill(this.colour);
+    rect(this.x, this.y, this.size, this.size);
+ }
+
+
+ }
 
 function Snake(){
     this.x = 10;
     this.y = 10;
-    this.xDir  = 1;
-    this.yDir = 0;
+    this.dir = "RIGHT";
     this.fed = false;
     this.cells = [];
 
@@ -57,7 +97,7 @@ function Snake(){
     text(this.cells.length, 5,10);
      if (ticks >= ticksCap){
     this.cells.forEach(cell => {
-            cell.move(this.xDir, this.yDir);
+            cell.move(this.dir);
         });
         ticks = 0;
         this.fed = false;
@@ -78,18 +118,14 @@ function Snake(){
      };
 
     this.changeDir = function(dir){
-        if (dir === "UP"){
-            this.xDir = 0;
-            this.yDir = -1;
-        }else if (dir === "DOWN"){
-            this.xDir = 0;
-            this.yDir = +1;
-        }else if (dir === "LEFT"){
-            this.xDir = -1;
-            this.yDir = 0;
-        }else if (dir === "RIGHT"){
-            this.xDir = 1;
-            this.yDir = 0;
+        if (dir === "UP" && this.dir !== "UP" && this.dir !== "DOWN" ){
+            this.dir = "UP";
+        }else if (dir === "DOWN"&& this.dir !== "DOWN" && this.dir !== "UP" ){
+            this.dir = "DOWN";
+        }else if (dir === "LEFT"&& this.dir !== "LEFT" && this.dir !=="RIGHT" ){
+            this.dir = "LEFT";
+        }else if (dir === "RIGHT"&& this.dir !== "RIGHT"&& this.dir !== "LEFT" ){
+            this.dir = "RIGHT";
         }
     };
 
@@ -111,14 +147,21 @@ function Cell(x,y,leader, food){
         rect(this.x, this.y, this.size, this.size);
     };
 
-    this.move = function(xDir, yDir){
+    this.move = function(dir){
         if (this.leader){
            this.x = this.leader.x;
            this.y = this.leader.y;
         }
         else {
-        this.x += xDir * 10;
-        this.y += yDir *10;
+            if (dir === "UP"){
+                this.y -= this.size;
+            }else if (dir === "DOWN"){
+                this.y += this.size;
+            }else if (dir === "LEFT"){
+                this.x -= this.size;
+            }else if (dir === "RIGHT"){
+                this.x += this.size;
+            }
         }
 
 }
