87526ffe37565b801bd01fa3a1101aa5d2995498
matt <27matt37@gmail.com>
2019-01-06 11:53:29 +0000

    ball

--- p5/brickBreaker/sketch.js
+++ p5/brickBreaker/sketch.js
@@ -1,14 +1,13 @@
-let y = 200;
-let x = 5;
 function setup() {
   // put setup code here
   createCanvas(400,400);
   background(230);
   text('I love you Katie', 10, 60);
   fill(0, 102, 153, 51);
-  let x = 20;
-  let y = 20;
   frameRate(60);
+
+  ball = new Ball(200,380);
+  // brick = new Brick(200,100);
 
 }
 
@@ -16,31 +15,66 @@ function draw() {
   // put drawing code here
   fill(20);
   background(230);
-  rect(x,y, 60, 60, 50);
-  text(x, 20, 20);
-  text(y, 20, 50);
+  ball.draw();
+  ball.move();
+  // brick.draw();
 
+  // brick.hit(ball.x, ball.y)
+  // also pass size of ball
 
 }
 
 
 function Ball (x,y){
+  this.x = x;
+  this.y = y;
+  this.stepx = 2;
+  this.stepy = 2;
+  this.angle = 135;
+  this.size = 10;
 
-this.stepx = 2;
-this.stepy = 2;
+  this.move = function(){
+    if (this.y >= 400 -this.size){
+      this.stepy *=-1;
+    }else if (this.y <= 0){
+      this.stepy*=-1;
+    }
+    if (this.x >= 400 - this.size){
+      this.stepx *= -1;
+    }else if (this.x<=0){
+      this.stepx *= -1;
+    }
+    this.y+= this.stepy;
+    this.x+=this.stepx;
+  }
 
-  this.move (){
-if (y >= 400 -60 ){
-  stepy *=-1;
-  }else if (y <= 0){
-    stepy*=-1;
+  this.draw = function (){
+    rect(this.x, this.y,this.size,this.size,this.size );
   }
-  if (x >= 400 - 60){
-  stepx *= -1;
-  }else if (x<=0){
-  stepx *= -1;
+}
+
+function Brick (x,y, durability){
+  this.x = x;
+  this.y = y;
+  this.colour = "rgb(0,255,0)";
+  this.durability = 1;
+  this.width = 50;
+  this.height = 20;
+
+  this.hit = function(attacker){
+    if (attacker.x >= this.x && attacker.x <= this.x + this.width && attacker.y >= this.y && attacker.y <= thisy + this.width ){
+
+
+      this.durability --;
+      if (this.durability === 0){
+        // remove 
+        this.colour = "rgb(255,255,0)";
+      }
+    }
   }
-  y+= stepy;
-  x+=stepx;
+
+  this.draw = function(){
+    fill(this.colour);
+    rect (this.x, this.y, this.width, this.height);
   }
 }
