c9e16e0de7d8254e0b07c9a4d5789a6a6656528a
matt <27matt37@gmail.com>
2019-01-12 18:10:50 +0100

    game of life done

--- p5/game-of-life/sketch.js
+++ p5/game-of-life/sketch.js
@@ -1,7 +1,3 @@
-let y = 200;
-let x = 5;
-let stepx = 2;
-let stepy = 2;
 function setup() {
   // any live cell with fewer than two live neighbors dies. underpopulation
   // any live cell with two or three live neighbors stay alive to next gen
@@ -12,9 +8,11 @@ function setup() {
   background(230);
   text('I love you Katie', 10, 60);
   fill(0, 102, 153, 51);
-  let x = 20;
-  let y = 20;
-  frameRate(60);
+  frameRate(5);
+
+  timer =  35;
+
+  ticks = 0;
 
   board = new Board();
   board.setup();
@@ -28,6 +26,17 @@ function draw() {
 
   board.draw();
 
+    if (ticks > timer){
+      board.calculateNeighbors();
+      board.game();
+      ticks = 0;
+      timer = 3;
+    }
+  ticks ++;
+
+  fill(255);
+  text(ticks, 20, 150);
+
 }
 
 function Board (){
@@ -35,15 +44,84 @@ function Board (){
 
   this.setup = function (){
     for (let j = 0; j < 40; j ++ ){
+      this.cells.push([]);
       for(let i = 0; i < 40; i ++ ){
-        this.cells.push(new Cell(i,j));
+        this.cells[j].push(new Cell(i,j));
       }
     }
   }
 
   this.draw= function(){
-    this.cells.forEach(cell => cell.draw());
+    this.cells.forEach(cellRow => {
+      cellRow.forEach(cell => cell.draw());
+    });
+  };
+
+  this.calculateNeighbors = function (){
+
+    for (let j = 1; j < 39; j ++ ){
+      for(let i = 1; i < 39; i ++ ){
+        let neighbors = 0;
+        fill(255);
+        if (this.cells[j][i].alive){
+
+        
+        text(this.cells[j][i].alive, 20, 300);
+        }
+        if ( j > 0 &&this.cells[j-1][i].alive ){
+          neighbors ++;
+        }
+        if ( j < 40 &&this.cells[j+1][i].alive ){
+          neighbors ++;
+        }
+        if ( i < 40 &&this.cells[j][i + 1].alive ){
+          neighbors ++;
+        }
+        if ( i > 0 &&this.cells[j][i - 1].alive ){
+          neighbors ++;
+        }
+        if ( j > 0 && i > 0&& this.cells[j-1][i - 1].alive ){
+          neighbors ++;
+        }
+        if ( j > 0 && i < 40 &&this.cells[j-1][i + 1].alive ){
+          neighbors ++;
+        }
+        if ( j < 40 && i > 0 &&this.cells[j+1][i-1].alive ){
+          neighbors ++;
+        }
+        if ( j < 40 && i < 40 &&this.cells[j+1][i+1].alive ){
+          neighbors ++;
+        }
+        textSize(30);
+        text(neighbors, 20, 100);
+        textSize(10);
+        this.cells[j][i].neighbors = neighbors;
+        
+        // can do this at setup. saving who are neighbors in Cell for faster
+      }
+    }
   }
+
+  this.game = function (){
+
+    this.cells.forEach(cellRow => {
+      cellRow.forEach(cell =>{
+        if (cell.neighbors < 2){
+          cell.alive = false;
+        }else if (cell.neighbors < 4){
+          cell.alive = true;
+        }else{
+          cell.alive = false;
+        }
+      });
+    });
+  }
+
+  this.pressed = function(){
+    this.cells.forEach(cellRow => {
+      cellRow.forEach(cell => cell.checkPressed());
+    });
+  };
 }
 
 function Cell (posX, posY){
@@ -52,10 +130,32 @@ function Cell (posX, posY){
   this.y = posY * this.size;
   this.colour = "rgb(50,50,50)";
   this.alive = false;
-  
+  this.neighbors = 0;
+
   this.draw = function(){
+    if (!this.alive){
+
+      this.colour = "rgb(50,50,50)";
+    }
+    else{
+
+      this.colour = "rgb(250,220,50)";
+    }
+
+    fill("rgb(0,255,255)");
+    text(this.neighbors, this.x, this.y);
     fill(this.colour);
     rect(this.x, this.y, this.size, this.size);
   }
 
+  this.checkPressed = function(){
+    if (mouseX > this.x && mouseX < this.x + this.size && mouseY > this.y && mouseY < this.y + this.size){
+      this.alive = true;
+    }
+  }
 }
+
+  function mouseReleased(){
+    board.pressed();
+
+  }
