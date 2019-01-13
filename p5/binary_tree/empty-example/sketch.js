17b80c987108bf4dbcb7f107bc7b325dcafedf53
matt <27matt37@gmail.com>
2019-01-13 12:08:26 +0000

    binary tree start

---
+++ p5/binary_tree/empty-example/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  // put setup code here
+  createCanvas(400,400);
+  background(230);
+  frameRate(60);
+
+}
+
+function draw() {
+  // put drawing code here
+  fill(20);
+  background(230);
+}
---
+++ p5/binary_tree/sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  // put setup code here
+  createCanvas(400,400);
+  background(230);
+  text('I love you Katie', 10, 60);
+  frameRate(60);
+
+}
+
+function draw() {
+  // put drawing code here
+  fill(20);
+  background(230);
+
+}
+
+function Node(head, left, right){
+  this.head = head;
+  this.left = left || null;
+  this.right = right || null;
+  
+  this.addItem = function (num){
+    if (num <= head){
+      this.left.addItem(num);
+    }else{
+      this.right.addItem(num);
+    }
+  }
+
+
+}
